887421cbf84bc8e685f3a7e6341332b63c3e3c53
Lukeveille <luke@lukeleveille.com>
2020-04-04 13:32:51 -0400

    Initial commit

---
+++ precache-manifest.86910d7b853646c2d90f3569ffdae632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201d6d95422f889ab44b89f3ea4f628d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c8e779b4eceb2be3a40",
+    "url": "./static/css/main.ff49a3a4.chunk.css"
+  },
+  {
+    "revision": "ed044680a6e55ce96b6a",
+    "url": "./static/js/2.8895a2f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8895a2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8e779b4eceb2be3a40",
+    "url": "./static/js/main.8f2aaaa8.chunk.js"
+  },
+  {
+    "revision": "c4463a1838dbc735169f",
+    "url": "./static/js/runtime-main.3c724f80.js"
+  }
+]);
